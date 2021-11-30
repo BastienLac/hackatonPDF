91d3511af56c8fab1801d03cde7cd9ef8b75b560
Bastien LACOUR <blacour@lerebours.org>
2021-11-30 17:01:12 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+// Import the main component
+import { Viewer } from '@react-pdf-viewer/core'; // install this library
+// Plugins
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // install this library
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+// Worker
+import { Worker } from '@react-pdf-viewer/core'; // install this library
 
-function App() {
+export const App = () => {
+
+  // Create new plugin instance
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  
+  // for onchange event
+  const [pdfFile, setPdfFile]=useState(null);
+  const [pdfFileError, setPdfFileError]=useState('');
+
+  // for submit event
+  const [viewPdf, setViewPdf]=useState(null);
+
+  // onchange event
+  const fileType=['application/pdf'];
+  const handlePdfFileChange=(e)=>{
+    let selectedFile=e.target.files[0];
+
+    alert(selectedFile.name);
+
+    if(selectedFile){
+      if(selectedFile&&fileType.includes(selectedFile.type)){
+        let reader = new FileReader();
+            reader.readAsDataURL(selectedFile);
+            reader.onloadend = (e) =>{
+              setPdfFile(e.target.result);
+              setPdfFileError('');
+            }
+      }
+      else{
+        setPdfFile(null);
+        setPdfFileError('Please select valid pdf file');
+      }
+    }
+    else{
+      console.log('select your file');
+    }
+  }
+
+  // form submit
+  const handlePdfFileSubmit=(e)=>{
+    e.preventDefault();
+    if(pdfFile!==null){
+      setViewPdf(pdfFile);
+    }
+    else{
+      setViewPdf(null);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+    <br></br>
+    
+      <form className='form-group' onSubmit={handlePdfFileSubmit}>
+        <input type="file" className='form-control'
+          required onChange={handlePdfFileChange}
+        />
+        {pdfFileError&&<div className='error-msg'>{pdfFileError}</div>}
+        <br></br>
+        <button type="submit" className='btn btn-success btn-lg'>
+          UPLOAD
+        </button>
+      </form>
+      <br></br>
+      <h4>View PDF</h4>
+      <div className='pdf-container' >
+        {/* show pdf conditionally (if we have one)  */}
+        {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+          <Viewer fileUrl={viewPdf}
+            plugins={[defaultLayoutPluginInstance]} />
+      </Worker></>}
+
+      {/* if we dont have pdf or viewPdf state is null */}
+      {!viewPdf&&<>No pdf file selected</>}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+/*const doc = await PDFNet.PDFDoc.createFromURL(filename);
+const page_num = await doc.getPageCount();
+for (let i=1; i<=page_num; ++i)
+{
+
+
+
+  const newDoc = await PDFNet.PDFDoc.create();
+  newDoc.insertPages(0, inDoc, i, i, PDFNet.PDFDoc.InsertFlag.e_none);
+  const buf = await newDoc.saveMemoryBuffer(PDFNet.SDFDoc.SaveOptions.e_linearized);
+
+  //optionally save the blob to a file or upload to a server
+  const blob = new Blob([buf], { type: 'application/pdf' });
+}*/
