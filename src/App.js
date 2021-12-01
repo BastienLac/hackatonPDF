ec6fe0f5c52511b19cd017d8abe29fae72b0ee92
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2021-12-01 09:01:00 +0100

    modif graphique sur app.js

--- src/App.js
+++ src/App.js
@@ -9,88 +9,94 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 // Worker
 import { Worker } from '@react-pdf-viewer/core'; // install this library
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { Container, Table, Navbar, Nav, Form} from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
+
+
 export const App = () => {
-
-  // Create new plugin instance
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-  
-  // for onchange event
-  const [pdfFile, setPdfFile]=useState(null);
-  const [pdfFileError, setPdfFileError]=useState('');
-
-  // for submit event
-  const [viewPdf, setViewPdf]=useState(null);
-
-  // onchange event
-  const fileType=['application/pdf'];
-  const handlePdfFileChange=(e)=>{
-    let selectedFile=e.target.files[0];
-
-    alert(selectedFile.name);
-
+    
+    // Create new plugin instance
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    
+    // for onchange event
+    const [pdfFile, setPdfFile]=useState(null);
+    const [pdfFileError, setPdfFileError]=useState('');
+    
+    // for submit event
+    const [viewPdf, setViewPdf]=useState(null);
+    
+    // onchange event
+    const fileType=['application/pdf'];
+    const handlePdfFileChange= (e) => {
+    const selectedFile = e.target.files[0];
+        
     if(selectedFile){
-      if(selectedFile&&fileType.includes(selectedFile.type)){
-        let reader = new FileReader();
+        if(selectedFile&&fileType.includes(selectedFile.type)){
+            let reader = new FileReader();
             reader.readAsDataURL(selectedFile);
             reader.onloadend = (e) =>{
-              setPdfFile(e.target.result);
-              setPdfFileError('');
+                setPdfFile(e.target.result);
+                setPdfFileError('');
             }
-      }
-      else{
-        setPdfFile(null);
-        setPdfFileError('Please select valid pdf file');
-      }
+        }
+        else{
+            setPdfFile(null);
+            setPdfFileError('Please select valid pdf file');
+        }
+      
     }
     else{
-      console.log('select your file');
+        console.log('select your file');
     }
-  }
+}
 
-  // form submit
-  const handlePdfFileSubmit=(e)=>{
+// form submit
+const handlePdfFileSubmit=(e)=>{
     e.preventDefault();
     if(pdfFile!==null){
-      setViewPdf(pdfFile);
+        setViewPdf(pdfFile);
     }
     else{
       setViewPdf(null);
     }
-  }
+}
+
 
   return (
-    <div className='container'>
+        <>
+            <div className="primary" style={{backgroundColor: 'blue', height: '50px', width: "100%"}}></div>
+            <div>
+                <Image src="https://alexlegal.fr/logo.jpg" style={{width: '150px'}} />  
+                <div>
+                    <form className='form-group mb-3 m-auto mb-5' onSubmit={handlePdfFileSubmit} style={{width: '40%'}}>
+                        <input type="file" className='form-control' required onChange={handlePdfFileChange} />
+                        {pdfFileError&&<div className='error-msg'>{pdfFileError}</div>}
+                        <button type="submit" className='btn btn-outline-primary mt-4 offset-5'>Enregister</button>
+                    </form>
+                </div>
+                <Button className="btn btn-outline-primary mt-5 offset-5" variant="outline-primary">Télecharger les fichiers</Button>{' '}
+            </div>
+            <Container className="w-50">
+                <label className="mt-5 text-center">Fichier PDF</label>
+                <div className='pdf-container mt-3' >
+                    {/* show pdf conditionally (if we have one)  */}
+                    {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+                    <Viewer fileUrl={viewPdf}
+                        plugins={[defaultLayoutPluginInstance]} />
+                    </Worker></>}
 
-    <br></br>
-    
-      <form className='form-group' onSubmit={handlePdfFileSubmit}>
-        <input type="file" className='form-control'
-          required onChange={handlePdfFileChange}
-        />
-        {pdfFileError&&<div className='error-msg'>{pdfFileError}</div>}
-        <br></br>
-        <button type="submit" className='btn btn-success btn-lg'>
-          UPLOAD
-        </button>
-      </form>
-      <br></br>
-      <h4>View PDF</h4>
-      <div className='pdf-container' >
-        {/* show pdf conditionally (if we have one)  */}
-        {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-          <Viewer fileUrl={viewPdf}
-            plugins={[defaultLayoutPluginInstance]} />
-      </Worker></>}
-
-      {/* if we dont have pdf or viewPdf state is null */}
-      {!viewPdf&&<>No pdf file selected</>}
-      </div>
-
-    </div>
-  )
+                    {/* if we dont have pdf or viewPdf state is null */}
+                    {!viewPdf&&<>Pas de pdf sélectioné</>}
+                </div>
+            </Container>
+        </>
+    )
 }
 
 export default App
+
 
 /*const doc = await PDFNet.PDFDoc.createFromURL(filename);
 const page_num = await doc.getPageCount();
